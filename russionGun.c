50c498eeee5f2fdaef80dc396fd31100823fc25f
Mohamed Mrabet <81630459+Mohamedazizmrabet@users.noreply.github.com>
2024-09-23 15:47:28 +0100

    russion semi done

--- russionGun.c
+++ russionGun.c
@@ -2,29 +2,74 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main(){
+int main() {
     int rounds;
     int table[6];
-   rounds= saisir(rounds);
-   remplirT();
-    printf("test");
+    rounds = saisir(rounds);
+    remplirT(table, rounds);
+    affichageT(table);
+    printf("test\n");
     return 0;
 }
-int saisir(){
+
+int saisir() {
     int x;
-    printf("saisir rounds : ");
-    scanf("%d",&x);
-    while(x>6 || x<1){
-       printf("saisir rounds : ");
-      scanf("%d",&x);
-    };
-return x;
+
+    do {
+        printf("Saisir rounds (1-6): ");
+        scanf("%d", &x);
+    } while (x < 1 || x > 6);
+
+    return x;
 }
 
-int remplirT(){
+int remplirT(int t[6], int rounds) {
+    int filledSlots = 0;
+
     srand(time(0));
-    int x=(rand()% 6);
-    printf("randommmmmmmmm %d",x);
+
+    while (filledSlots < rounds) {
+        int x = rand() % 6;
+
+        if (t[x] == 0) {
+            t[x] = 1;
+            filledSlots++;
+        }
+    };
+   for (int i = 0; i < 6; i++) {
+        if(t[i]!=1){
+            t[i]=0;
+        }
+    };
+
+
     return 0;
 }
 
+int verif(int t[6], int rounds) {
+    int filledSlots = 0;
+
+    for (int i = 0; i < 6; i++) {
+        if (t[i] == 1) {
+            filledSlots++;
+        }
+    }
+
+    return filledSlots == rounds;
+}
+
+int affichageT(int t[6]) {
+    for (int i = 0; i < 6; i++) {
+        printf("Element of the table %d: %d\n", i + 1, t[i]);
+    }
+
+    return 0;
+}
+
+int play (int t[6],int rounds){
+int i=0;
+while(i<6){
+
+}
+
+}
